133814f541ddf70e4c4b1209606d1300ae570b2d
ishita <ishitajha26@gmail.Com>
2025-01-27 12:22:05 +0530

    food application

---
+++ app.js
@@ -0,0 +1,22 @@
+let reviews = document.querySelectorAll(".review-wrapper");
+
+let currentReviews = [0, 2];
+let updateReviewSlider = (cards) => {
+  cards.forEach((card_index) => {
+    reviews[card_index].classList.add("active");
+  });
+};
+
+setInterval(() => {
+  currentReviews.forEach((card_index, i) => {
+    reviews[card_index].classList.remove("active");
+
+    currentReviews[i] = card_index >= reviews.length - 1 ? 0 : card_index + 1;
+  });
+
+  setTimeout(() => {
+    updateReviewSlider(currentReviews);
+  }, 250);
+}, 5000);
+
+updateReviewSlider(currentReviews);
