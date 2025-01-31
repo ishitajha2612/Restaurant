927169102cbcdd46aa55452d4cba35f7805ec622
ishita <ishitajha26@gmail.Com>
2025-01-31 12:04:53 +0530

    responsive website

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-let reviews = document.querySelectorAll(".review-wrapper");
+//nav toggler
 
+let navToggler = document.querySelector('.nav-toggler');
+let linksContainer = document.querySelector('.links-container')
+
+navToggler.addEventListener('click', () => {
+  navToggler.classList.toggle('active');
+  linksContainer.classList.toggle('active');
+})
+
+if (linksContainer.classList.contains("active")) {
+  navToggler.innerHTML = '<i class="fa-solid fa-times"></i>'; // Close icon
+} else {
+  navToggler.innerHTML = '<i class="fa-solid fa-bars"></i>'; // Hamburger icon
+}
+
+
+let reviews = document.querySelectorAll(".review-wrapper");
+   
 let currentReviews = [0, 2];
 let updateReviewSlider = (cards) => {
   cards.forEach((card_index) => {
@@ -26,10 +43,24 @@ updateReviewSlider(currentReviews);
 //faq
 
 let faqs = [...document.querySelectorAll(".faq")];
-faqs.forEach((faq) => {
+faqs.map((faq) => {
   let ques = faq.querySelector(".question-box");
 
   ques.addEventListener("click", () => {
     faq.classList.toggle("active");
   });
 });
+
+
+// dish slider
+
+let dishSlider = document.querySelector('.dish-slide')
+let rotationVal = 0;
+setInterval(()=> {
+  rotationVal += 120;
+  dishSlider.style.transform=`translateY(-50%) rotate(${rotationVal}deg)`
+
+  
+}, 3000)
+
+AOS.init();
